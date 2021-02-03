c3b7038dbd3ac72306c9d253e2475ea4c8453643
Dave Lobban <dave.lobban@atmosi.com>
2021-02-03 16:00:24 +0000

    Add Pipeline Page & initial tests

--- Ui.Tests/UnitTest1.cs
+++ Ui.Tests/UnitTest1.cs
@@ -1,14 +1,65 @@
 using System;
+using System.Linq;
 using Xunit;
+using Bunit;
+using System.Threading.Tasks;
+using AngleSharp.Diffing.Core;
+using AngleSharp.Dom;
+using Ui.Pages;
+using Xunit.Abstractions;
 
-namespace Ui.Tests
+namespace ui.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
 
+
+    public class PipelinePageTests : TestContext
+    {
+        public PipelinePageTests(ITestOutputHelper output) { this.output = output; }
+
+        private readonly ITestOutputHelper output;
+
+        [Fact]
+        public void PipelineComponent_ShowsTheDefaultPipelineName()
+        {
+            var cut = RenderComponent<PipelinePage>();
+
+            var pipelineNameElem = cut.Find("#pipeline-name");
+            Assert.Contains(pipelineNameElem.InnerHtml, "Name: NewPipeline1");
+        }
+
+
+        [Fact]
+        public void PipelineComponent_LetsUserUpdateNameAndWhenSaveIsClickedShowsTheUpdatedName()
+        {
+            var cut = RenderComponent<PipelinePage>();
+
+            var pipelineNameElem = cut.Find("#pipeline-name");
+            Assert.Contains(pipelineNameElem.InnerHtml, "Name: NewPipeline1");
+
+            var nameEditingDiv = cut.Find("#name-editing");
+            AssertHidden(nameEditingDiv);
+
+            var editButton = cut.Find("#pipeline-name-div button");
+            editButton.Click();
+            AssertVisible(nameEditingDiv);
+
+            var newPipelineName = "Edited Name 001";
+            cut.Find("#name-editing input").Change(newPipelineName);
+
+            cut.Find("#pipeline-name-edit-apply").Click();
+            AssertHidden(nameEditingDiv);
+            Assert.Contains(pipelineNameElem.InnerHtml, $"Name: {newPipelineName}");
+
+        }
+
+        private static void AssertVisible(IElement expectedHidden)
+        {
+            Assert.True(expectedHidden.Attributes.Count(a => a.Name == "hidden" && a.Value == "false") == 0);
+        }
+
+        private static void AssertHidden(IElement expectedHidden)
+        {
+            Assert.True(expectedHidden.Attributes.Count(a => a.Name == "hidden" && a.Value == "") == 1);
         }
     }
 }
--- Ui/Startup.cs
+++ Ui/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MatBlazor;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +31,7 @@ namespace Ui
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<SlowWeatherForecastService>();
+            services.AddMatBlazor();
 
         }
 
